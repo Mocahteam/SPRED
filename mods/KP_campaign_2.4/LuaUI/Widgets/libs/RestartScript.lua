4744671941985235aa0d4f091f3d386d4295ed62
Bruno Martin <martin.bruno.mail@gmail.com>
2016-02-06 21:49:53 +0100

    restart script updated

--- mods/KP_campaign_2.4/LuaUI/Widgets/libs/RestartScript.lua
+++ mods/KP_campaign_2.4/LuaUI/Widgets/libs/RestartScript.lua
@@ -1,13 +1,13 @@
-function DoTheRestart(startscriptfilename, lang, scenario)
+function DoTheRestart(startscriptfilename, tableOperation)
+  -- Warning : tableOperation must not include keys which are a substring of another key in the txt file
+  -- for exemple, as it happened, using a key such as mode when gamemode already exists.
+  -- One idea would be to fix how regex are made in the function replace section.
 	local n_infologcopy="infolog.bak.txt"
+	Spring.Echo("test")
 	Spring.Echo(widget:GetInfo().name..": Wanting to reload \""..startscriptfilename.."\"")
 	if VFS.FileExists(startscriptfilename) then
 		local complete=VFS.LoadFile(startscriptfilename)
-		Spring.Echo(widget:GetInfo().name..": that file contains:")
-		complete,_=string.gsub(complete,"language=[^;]*;","language="..lang..";",1)
-		if scenario ~= nil then
-			complete,_=string.gsub(complete,"scenario=[^;]*;","scenario="..scenario..";",1)
-		end
+    complete=updateValues(complete, tableOperation)
 		Spring.Echo("\n\n<<BEGIN DUMPING FULL \""..startscriptfilename.."\">>\n\n")
 		Spring.Echo(complete)
 		Spring.Echo("\n\n<<END DUMPING FULL \""..startscriptfilename.."\">>\n\n")
@@ -33,4 +33,34 @@ function DoTheRestart(startscriptfilename, lang, scenario)
 	else
 		Spring.Echo(widget:GetInfo().name..": that file is not valid... Restart aborted")
 	end
+end
+
+function replaceSection(fullfile,sectionName,replacement)
+    local regexSection="%["..sectionName.."%]%s*%{([^%}]*)%}"
+    local regexBracket="%}"
+    local indexSection=string.find(fullfile,regexSection)
+    local indexOfNextClosingBracked=string.find(fullfile,regexBracket,indexSection)
+    local firstPart=string.sub(fullfile, 1,indexSection-1)
+    local lastPart=string.sub(fullfile, indexOfNextClosingBracked+1)
+    local fullstring=firstPart..replacement..lastPart
+    return(fullstring)
+end
+
+function updateValues(fullFile,tableOperation)
+  for section,operations in pairs(tableOperation) do
+    local regexSection="%["..section.."%]%s*%{([^%}]*)%}"
+    local contentSection=string.match(fullFile,regexSection) -- contain the content of the section
+    local oldSection=contentSection
+    for attribute,value in pairs(operations) do
+      if(string.match(contentSection,attribute.."=[^;]*;")) then
+        contentSection,_=string.gsub(contentSection,attribute.."=[^;]*;",attribute.."="..value..";",1)
+      else
+        contentSection=contentSection.."\r\n\t\t"..attribute.."="..value..";"
+      end
+    end
+    contentSection="["..section.."]\r\n\t{"..contentSection.."\r\n\t}"
+    fullFile=replaceSection(fullFile,section,contentSection)
+  end
+    --local fullFile=string.gsub(fullFile,"%["..section.."%]%s*%{([^%}]*)%}",contentSection) -- replace the content of the action
+  return fullFile 
 end
