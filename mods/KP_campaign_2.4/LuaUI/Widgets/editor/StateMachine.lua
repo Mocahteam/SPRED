608535ebce0d5962fa9dd03846aa2c6725fcf1f7
Benjamin BONTEMPS <benjamin.bontemps@telecom-paristech.fr>
2016-02-09 11:13:32 +0100

    auto generation of buttons/functions works
    Former-commit-id: 85f6dfa5328c3748fd89b4a96aa0b700efeb76de

--- mods/KP_campaign_2.4/LuaUI/Widgets/editor/StateMachine.lua
+++ mods/KP_campaign_2.4/LuaUI/Widgets/editor/StateMachine.lua
@@ -5,9 +5,10 @@ StateMachine = {}
 StateMachine.__index = StateMachine
 
 -- Constructor
-function StateMachine.new(_states, _currentGlobalState, _currentUnitState, _currentTeamState)
+function StateMachine.new(_states, _unitStates, _currentGlobalState, _currentUnitState, _currentTeamState)
 	local self = setmetatable({}, StateMachine)
 	self.states = _states
+	self.unitStates = _unitStates
 	self.currentGlobalState = _currentGlobalState
 	self.currentUnitState = _currentUnitState
 	self.currentTeamState = _currentTeamState
@@ -25,14 +26,30 @@ function StateMachine.getCurrentTeamState(self) return self.currentTeamState end
 function StateMachine.getCurrentGlobalState(self) return self.currentGlobalState end
 
 ------------------------------
--- Initialisation of the state machine
+-- List of all the units of the game
 -- TODO : generation of the table using constant list
 ------------------------------
+unitList = {"bit", "byte", "assembler", "alice"}
+
+------------------------------
+-- Initialisation of the state machine
+------------------------------
+
+-- states to select the unit to place on the field
+local unitStates = {}
+for i, u in ipairs(unitList) do
+	unitStates[u] = u
+	if i == 1 then
+		unitStates.DEFAULT = u
+	end
+end
+
+-- other states
 local states = {	-- Global
-						FILE = "file", UNIT = "unit", SELECTION = "selection",
-						-- Units
-						BIT = "bit", BYTE = "byte",
+						FILE = "file", UNIT = "unit", SELECTION = "selection", EVENT = "event", ACTION = "action", LINK = "link",
 						-- Teams
 						PLAYER = "0", ALLY = "1", ENEMY = "2"
 					}
-stateMachine = StateMachine.new(states, states.FILE, states.BIT, states.PLAYER)
+
+-- init
+stateMachine = StateMachine.new(states, unitStates, states.FILE, unitStates.DEFAULT, states.PLAYER)
