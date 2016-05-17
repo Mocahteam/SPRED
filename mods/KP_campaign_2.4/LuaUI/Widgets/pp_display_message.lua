4b8ff8084eb0b3a954895b66e96bebbdd46530b0
Bruno Martin <martin.bruno.mail@gmail.com>
2016-05-17 16:57:38 +0200

    multiple line is implemented in displayMessage, a temporary version of mission3 is built to test it
    Former-commit-id: 996ceb811cf50f864f8eda286a935b4f71be7b43

--- mods/KP_campaign_2.4/LuaUI/Widgets/pp_display_message.lua
+++ mods/KP_campaign_2.4/LuaUI/Widgets/pp_display_message.lua
@@ -2,7 +2,7 @@ function widget:GetInfo()
   return {
     name      = "Display Message",
     desc      = "Display message at a specific position or above a unit",
-    author    = "zigaroula",
+    author    = "zigaroula,martinb",
     date      = "Apr 29, 2016",
     license   = "GPL v2 or later",
     layer     = 0,
@@ -13,6 +13,7 @@ end
 local UnitMessages = {}
 local PositionMessages = {}
 
+
 function DisplayMessageAboveUnit(message, unit, timer)
 	table.insert(UnitMessages, {message = message, unit = unit, timer = timer,infinite=(timer==0)})
 end
@@ -21,21 +22,50 @@ function DisplayMessageAtPosition(message, x, y, z, timer)
 	table.insert(PositionMessages, {message = message, x = x, y = y, z = z, timer = timer,infinite=(timer==0)})
 end
 
+function TurnToIntoLines(texte)
+  local maxWidth=10
+  local tableLines={}
+  local message=""
+  local wCurrentLine=0
+  local f=true--first insert
+  for token in string.gmatch(texte, "[^%s]+") do
+    if(((wCurrentLine+gl.GetTextWidth(token))<maxWidth) or (message=="")) then -- we continue to write on the same line
+      if (f) then message=token ; f=false -- to avoid a blank space at the beginning of the message
+      else message=message.." "..token end
+      wCurrentLine=wCurrentLine+gl.GetTextWidth(token)
+    else -- threshold reached => newline
+      table.insert(tableLines,message)
+      message=token
+      wCurrentLine=0
+    end
+  end
+  table.insert(tableLines,message) -- to include the tail of the string for which, by definition, the threshold is not reached
+  return tableLines
+end
+
+-- display text at position, if the text is too wide
+-- it will be cut in smaller parts by the call of TurnToIntoLines
+function DisplayTextAtScreenPosition(x, y, texte)
+    local s = 30
+    local tableLine=TurnToIntoLines(texte)
+    local nLines=table.getn(tableLine)
+    for i, m in ipairs(tableLine) do
+      local w = gl.GetTextWidth(texte)*s
+      gl.Text(m, x-50, y+(nLines-i)*s, s, "s")
+    end
+end
+
 function widget:DrawScreen()
 	for i, mes in ipairs(UnitMessages) do
 		local u, m = mes.unit, mes.message
 		local x, y, z = Spring.GetUnitPosition(u)
 		x, y = Spring.WorldToScreenCoords(x, y+50, z)
-		local s = 30
-		local w = gl.GetTextWidth(m)*s
-		gl.Text(m, x - (w/2), y, s, "s")
+		DisplayTextAtScreenPosition(x, y, m)
 	end
 	for i, mes in ipairs(PositionMessages) do
 		local x, y, z, m = mes.x, mes.y, mes.z, mes.message
 		x, y = Spring.WorldToScreenCoords(x, y, z)
-		local s = 30
-		local w = gl.GetTextWidth(m)*s
-		gl.Text(m, x - (w/2), y, s, "s")
+    DisplayTextAtScreenPosition(x, y, m)
 	end
 end
 
